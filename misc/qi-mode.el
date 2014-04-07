ac54233bc6325da2830c8c1ee54d66177458bd84
Cedric GESTES <gestes@aldebaran-robotics.com>
2014-04-07 16:56:26 +0200

    emacs: better interface decl color
    color member name

--- misc/qi-mode.el
+++ misc/qi-mode.el
@@ -13,11 +13,18 @@
   '("if" "package" "end" "for"
     "interface" "fn" "in" "out" "prop"
     "object" "at")      ;; some keywords
-  '(("\\b[0-9]+\\b" . font-lock-constant-face)    ;; int constant
-    ("^[ ]*\\([a-zA-Z]+\\)[ ]*\\(:\\)"            ;; blabla: value
+  '(;; int constant
+    ("\\b[0-9]+\\b" . font-lock-constant-face)
+    ;; blabla: value
+    ("^[ ]*\\([a-zA-Z]+\\)[ ]*\\(:\\)"
      (1 font-lock-variable-name-face)
      (2 font-lock-keyword-face))
-    ("@[a-zA-Z]+" . font-lock-preprocessor-face)) ;; @attr
+    ;; @attr
+    ("@[a-zA-Z]+" . font-lock-preprocessor-face)
+    ;; fn/in/out/prop names
+    ("^[ ]*\\(fn\\|in\\|out\\|prop\\)[ ]*\\([a-zA-Z]+\\)"
+     2 font-lock-function-name-face))
+
   '("\\.qi$")                                     ;; files that trigger this mode
   nil                                             ;; any other functions to call
   "qilang highlighting mode"                      ;; doc string
