71c1c9d5f89408518d42f76f7a8567d1dedc9160
Cedric GESTES <gestes@aldebaran-robotics.com>
2014-04-17 18:13:17 +0200

    main: argument parsing
    Change-Id: I9ca29bc124e87c0764a803435c19d02587b4b4c4

--- src/qic_main.cpp
+++ src/qic_main.cpp
@@ -11,38 +11,59 @@
 #include <qilang/node.hpp>
 #include <qilang/parser.hpp>
 #include <qilang/formatter.hpp>
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
 
 int main(int argc, char *argv[])
 {
-  std::cout << "qi!" << std::endl;
-  std::istream *in;
-  std::ifstream is;
+  po::options_description desc("qilang options");
+  desc.add_options()
+      ("help,h", "produce help message")
+      ("codegen,c", po::value<std::string>()->default_value(""), "Set the codegenerator to use")
+      ("input-file", po::value< std::vector< std::string> >(), "input files")
+      ("output-dir,o", po::value<std::string>(), "output dir")
+      ;
 
-  if (argc == 1)
-    in = &std::cin;
-  else {
-    is.open(argv[1]);
-    in = &is;
+  po::positional_options_description p;
+  p.add("input-file", -1);
+
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+  po::notify(vm);
+
+
+  if (vm.count("help")) {
+      std::cout << desc << std::endl;
+      return 1;
   }
 
-  qilang::NodePtrVector rootnode;
-  try {
-    rootnode = qilang::parse(in, argv[1]);
-  } catch(const std::exception& e) {
-    std::cout << e.what() << std::endl;
+  std::string              codegen;
+  std::vector<std::string> files;
+
+  codegen = vm["codegen"].as<std::string>();
+  if (codegen != "cpp" && codegen != "qilang" && codegen != "sexpr") {
+    std::cout << "Invalid codegen value: use cpp/qilang/sexpr" << std::endl;
     exit(1);
   }
-  std::cout << "AST:" << std::endl;
-  std::cout << qilang::formatAST(rootnode);
 
-  std::cout << std::endl;
-  std::cout << "QILANG:" << std::endl;
-  std::cout << qilang::format(rootnode);
+  files = vm["input-file"].as< std::vector<std::string> >();
+  for (int i = 0; i < files.size(); ++i) {
+    std::cout << "Generating " << codegen << " for " << files.at(i) << std::endl;
 
-
-  std::cout << std::endl;
-  std::cout << "HPP:" << std::endl;
-  std::cout << qilang::genCppObjectInterface(rootnode);
-
+    qilang::NodePtrVector rootnode;
+    try {
+      rootnode = qilang::parse(files.at(i));
+    } catch(const std::exception& e) {
+      std::cout << e.what() << std::endl;
+      exit(1);
+    }
+    if (codegen == "cpp")
+      std::cout << qilang::genCppObjectInterface(rootnode);
+    else if (codegen == "qilang")
+      std::cout << qilang::format(rootnode);
+    else if (codegen == "sexpr")
+      std::cout << qilang::formatAST(rootnode);
+  }
   return 0;
 }
