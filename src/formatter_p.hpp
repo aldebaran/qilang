285d76c792b28f4f06fca9d2e4cf88d39986b0ef
Cedric GESTES <gestes@aldebaran-robotics.com>
2014-04-29 10:37:59 +0200

    formatter: clear header
    Change-Id: Ia830b15d788173bfea483eb96164ab592f13e98c

--- src/formatter_p.hpp
+++ src/formatter_p.hpp
@@ -1,26 +1,8 @@
 /*
-** formatter_p.hpp
-** Login : <ctaf@torchbook>
-** Started on  Wed Apr  9 14:14:10 2014
-** $Id$
-**
 ** Author(s):
-**  -  <gestes@aldebaran-robotics.com>
+**  - Cedric GESTES <gestes@aldebaran-robotics.com>
 **
-** Copyright (C) 2014
-** This program is free software; you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation; either version 3 of the License, or
-** (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+** Copyright (C) 2014 Aldebaran Robotics
 */
 
 #ifndef   	FORMATTER_P_HPP_
