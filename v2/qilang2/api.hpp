37a1bd0abec60b521dbbaa4415a7f53e4e26723b
Cedric GESTES <gestes@aldebaran-robotics.com>
2014-05-05 00:11:08 +0200

    add a v2 project to try defining qilang in qilang
    Change-Id: Ieed10ef4c30bf49f4cab45ba62cb9bfa31b92fb9

---
+++ v2/qilang2/api.hpp
@@ -0,0 +1,15 @@
+/*
+** Author(s):
+**  - Cedric GESTES <gestes@aldebaran-robotics.com>
+**
+** Copyright (C) 2014 Aldebaran Robotics
+*/
+
+#ifndef _QILANG2_API_HPP_
+#define _QILANG2_API_HPP_
+
+#include <qi/macro.hpp>
+
+#define QILANG2_API QI_LIB_API(qilang2)
+
+#endif
---
+++ v2/src/main.cpp
@@ -0,0 +1,21 @@
+/*
+** Author(s):
+**  - Cedric GESTES <gestes@aldebaran-robotics.com>
+**
+** Copyright (C) 2014 Aldebaran Robotics
+*/
+
+#include <iostream>
+#include <qilang2/node.hpp>
+
+
+
+int main(int argc, char *argv[])
+{
+  qilang2::ExprNode n = qilang2::newVarExprNode("plouf", qilang2::Location());
+
+  qilang2::Node n2 = n;
+
+  std::cout << "name:" << n->type() << std::endl;
+  return 0;
+}
