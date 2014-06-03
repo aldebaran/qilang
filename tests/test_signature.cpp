1049ae7a9e11a002c54075d5028796bc79bf13a8
Mehdi Ambari <mambari@aldebaran-robotic.com>
2014-06-03 15:25:57 +0200

    modif test signature
    Change-Id: I55294da152e28c5b44d95f3944f73c5e8d35ea58
    Reviewed-on: http://gerrit.aldebaran.lan/41390
    Reviewed-by: rvarannes <rvarannes@aldebaran-robotics.com>
    Approved: mambari <mambari@aldebaran-robotics.com>
    Reviewed-by: mambari <mambari@aldebaran-robotics.com>
    Tested-by: mambari <mambari@aldebaran-robotics.com>

--- tests/test_signature.cpp
+++ tests/test_signature.cpp
@@ -17,7 +17,7 @@ TEST(TestSignature, signatureToQiLangList) {
   const qi::Signature signature("([b][c][C][w][W][i][I][l][L][f][d][s][m])"); //miss v
   qilang::TypeExprNodePtr node = qilang::signatureToQiLang(signature);
 
-  EXPECT_STREQ("(bool[], int8[], uint8[], int16[], uint16[], int32[], uint32[], int64[], uint64[], float32[], float64[], str[], any[])", qilang::format(node).c_str());
+  EXPECT_STREQ("([]bool, []int8, []uint8, []int16, []uint16, []int32, []uint32, []int64, []uint64, []float32, []float64, []str, []any)", qilang::format(node).c_str());
 }
 
 TEST(TestSignature, signatureToQiLangTuple) {
@@ -39,26 +39,48 @@ qi::MetaMethod metaMethodeTest(){
   const qi::Signature parametersSignature("(bcCiIlLfdsm)");
   qi::MetaMethodParameterVector parameters;
   qi::MetaMethodParameter parameter1("Parameter1", "Parameter1 is a bool");
+  qi::MetaMethodParameter parameter2("Parameter2", "Parameter2 is a int8");
+  qi::MetaMethodParameter parameter3("parameter3", "parameter3 is a uint8");
+  qi::MetaMethodParameter parameter4("parameter4", "parameter4 is a int32");
+  qi::MetaMethodParameter parameter5("parameter5", "parameter5 is a uint32");
+  qi::MetaMethodParameter parameter6("parameter6", "parameter6 is a int64");
+  qi::MetaMethodParameter parameter7("parameter7", "parameter7 is a uint64");
+  qi::MetaMethodParameter parameter8("parameter8", "parameter8 is a float32");
+  qi::MetaMethodParameter parameter9("parameter9", "parameter9 is a float64");
+  qi::MetaMethodParameter parameter10("parameter10", "parameter10 is a str");
+  qi::MetaMethodParameter parameter11("parameter11", "parameter11 is a any");
+
   parameters.push_back(parameter1);
-  const qi::MetaMethod metamethode(100, returnSignature, "methodetoto", parametersSignature, "Methode test of metaMethodToQiLang", parameters, "Return int32");
+  parameters.push_back(parameter2);
+  parameters.push_back(parameter3);
+  parameters.push_back(parameter4);
+  parameters.push_back(parameter5);
+  parameters.push_back(parameter6);
+  parameters.push_back(parameter7);
+  parameters.push_back(parameter8);
+  parameters.push_back(parameter9);
+  parameters.push_back(parameter10);
+  parameters.push_back(parameter11);
+
+  const qi::MetaMethod metamethode(100, returnSignature, "methodeTest", parametersSignature, "Methode test of metaMethodToQiLang", parameters, "Return int32");
   return metamethode;
 }
 
 qi::MetaSignal metaSignalTest(){
   const qi::Signature signature("(c)");
-  const qi::MetaSignal signal(100, "signaltoto", signature);
+  const qi::MetaSignal signal(100, "signalTest", signature);
   return signal;
 }
 
 qi::MetaProperty metaPropertyTest(){
   const qi::Signature signature("(c)");
-  const qi::MetaProperty property(100, "propertytoto", signature);
+  const qi::MetaProperty property(100, "propertyTest", signature);
   return property;
 }
 
 TEST(TestSignature, metaObjectToQiLang) {
-  const std::string pkg = ("packageToto");
-  const std::string name = ("ObjectToto");
+  const std::string pkg = ("packageTest");
+  const std::string name = ("ObjectTest");
   //method
   qi::MetaObject::MethodMap methodMap;
   methodMap.insert(std::pair<unsigned int,qi::MetaMethod>(0, metaMethodeTest()));
@@ -72,7 +94,7 @@ TEST(TestSignature, metaObjectToQiLang) {
   const qi::MetaObject object(methodMap, signalMap, propertyMap, "Objet test of metaObjectToQiLang");
   qilang::NodePtr node = qilang::metaObjectToQiLang(name, object);
 
-  EXPECT_STREQ("interface ObjectToto\n  fn methodetoto(bool, int8, uint8, int32, uint32, int64, uint64, float32, float64, str, any) int32\n  out signaltoto(int8)\n  prop propertytoto(int8)\nend\n\n", qilang::format(node).c_str());
+  EXPECT_STREQ("interface ObjectTest\n  fn methodeTest(bool, int8, uint8, int32, uint32, int64, uint64, float32, float64, str, any) int32\n  out signalTest(int8)\n  prop propertyTest(int8)\nend\n\n", qilang::format(node).c_str());
 }
 
 int main(int argc, char **argv) {
