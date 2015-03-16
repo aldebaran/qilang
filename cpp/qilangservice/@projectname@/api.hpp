e00f3f9f7ebb95cb6674a5f6d58b4f5427e0d807
Philippe Daouadi <pdaouadi@aldebaran-robotics.com>
2015-03-16 15:26:22 +0100

    Add qilangservice template
    Change-Id: I786cf06f246e0f30c5e7698ea0ef3ce66459da56
    Reviewed-on: http://gerrit.aldebaran.lan/69341
    Reviewed-by: vpaleologue <vpaleologue@aldebaran-robotics.com>
    Tested-by: vpaleologue <vpaleologue@aldebaran-robotics.com>

---
+++ cpp/qilangservice/@projectname@/api.hpp
@@ -0,0 +1,14 @@
+/*
+**  Copyright (C) 2015 Aldebaran Robotics
+**  See COPYING for the license
+*/
+
+#pragma once
+
+#ifndef @PROJECTNAME@_API_HPP_
+#define @PROJECTNAME@_API_HPP_
+
+#include <qi/macro.hpp>
+#define @PROJECTNAME@_API QI_LIB_API(@projectname@)
+
+#endif // !@PROJECTNAME@_API_HPP_
